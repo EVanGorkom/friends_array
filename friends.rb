04f0daed137f149458f843961fc062007919fdb1
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-05-17 19:21:38 -0500

    Test array method

--- friends.rb
+++ friends.rb
@@ -2,3 +2,5 @@ friends = ["Ethan", "Kaylee", "Tommy", "Cydnee", "Kaitlyn"]
 puts friends [3]
 puts friends [4]
 puts friends [0]
+places_2_visit = ["Venice", "Australia", "Japan", "Scotland", "Ireland"]
+puts places_2_visit.shift
